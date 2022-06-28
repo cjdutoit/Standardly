1a5bbfa7fd0af43003dc36c60e6ac3fb671a6886
Christo du Toit <christo@dutoit.co.uk>
2022-06-28 22:22:46 +0100

    BROKERS: File System Broker - CheckIfFileExists

---
+++ Standardly.Core/Brokers/FileSystems/FileSystemBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.IO;
+
+namespace Standardly.Core.Brokers.FileSystems
+{
+    public class FileSystemBroker : IFileSystemBroker
+    {
+        public bool CheckIfFileExists(string path) =>
+            File.Exists(path);
+    }
+}
---
+++ Standardly.Core/Brokers/FileSystems/IFileSystemBroker.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Standardly.Core.Brokers.FileSystems
+{
+    public interface IFileSystemBroker
+    {
+        bool CheckIfFileExists(string path);
+    }
+}
