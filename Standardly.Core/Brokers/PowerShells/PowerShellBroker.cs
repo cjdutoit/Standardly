23d98e97dbb8568ad3219657179fecda002311c7
Christo du Toit <christo@dutoit.co.uk>
2022-06-28 23:46:21 +0100

    BROKERS: PowerShell - RunScript

---
+++ Standardly.Core/Brokers/PowerShells/PowerShellBroker.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using Standardly.Core.Models.PowerShellScripts;
+
+namespace Standardly.Core.Brokers.PowerShells
+{
+    public class PowerShellBroker : IPowerShellBroker
+    {
+        private RunspacePool RsPool { get; set; }
+
+        public string RunScript(List<PowerShellScript> scripts, string executionFolder)
+        {
+            using (PowerShell powershell = PowerShell.Create())
+            {
+                powershell
+                    .AddScript($"cd {executionFolder}");
+
+                foreach (PowerShellScript script in scripts)
+                {
+                    powershell.AddScript($"{script.Script}");
+                }
+
+                List<PSObject> PSOutput = powershell.Invoke().ToList();
+                var sb = new StringBuilder();
+
+                foreach (PSObject obj in PSOutput)
+                {
+                    if (obj != null)
+                    {
+                        try
+                        {
+                            string message = obj.Properties["Message"]?.Value.ToString();
+                            if (!string.IsNullOrWhiteSpace(message))
+                            {
+                                sb.AppendLine(message);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ Standardly.Core/Models/PowerShellScripts/PowerShellScript.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Standardly.Core.Models.PowerShellScripts
+{
+    public class PowerShellScript
+    {
+        public string Name { get; set; }
+        public string Script { get; set; }
+    }
+}
