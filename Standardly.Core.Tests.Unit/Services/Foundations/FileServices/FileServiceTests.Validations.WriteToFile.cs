c3c1eb7a52753e83765c381b204e55a5de0d8ad5
Christo du Toit <christo@dutoit.co.uk>
2022-06-30 00:07:49 +0100

    ShouldThrowValidationExceptionOnWriteToFileIfPathIsInvalid -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/FileServices/FileServiceTests.Validations.WriteToFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.FileServices.Exceptions;
+using Standardly.Core.Services.Foundations.FileServices;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.FileServices
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ShouldThrowValidationExceptionOnWriteToFileIfPathIsInvalid(
+            string invalidPath)
+        {
+            // given
+            string someContent = GetRandomString();
+
+            var invalidFilePathException =
+                new InvalidFilePathException();
+
+            var expectedFileServiceValidationException =
+                new FileServiceValidationException(invalidFilePathException);
+
+            // when
+            Action checkIfDirectoryExistsAction = () =>
+                this.fileService.WriteToFile(invalidPath, someContent);
+
+            FileServiceValidationException actualException = Assert.Throws<FileServiceValidationException>(
+                checkIfDirectoryExistsAction);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileServiceValidationException);
+
+            this.fileSystemBrokerMock.Verify(broker =>
+                broker.WriteToFile(invalidPath, someContent),
+                        Times.Never);
+
+            this.fileSystemBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
