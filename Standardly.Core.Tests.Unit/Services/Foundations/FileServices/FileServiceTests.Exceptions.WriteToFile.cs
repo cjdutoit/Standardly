091bc9edde1c6586d1456a5ffd3d0b219b7b4055
Christo du Toit <christo@dutoit.co.uk>
2022-06-30 00:17:27 +0100

    ShouldThrowDependencyValidationExceptionOnWriteIfDependencyValidationErrorOccursAndLogIt -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/FileServices/FileServiceTests.Exceptions.WriteToFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Moq;
+using Standardly.Core.Models.FileServices.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.FileServices
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyValidationExceptions))]
+        public void ShouldThrowDependencyValidationExceptionOnWriteIfDependencyValidationErrorOccursAndLogIt(
+            Exception dependencyValidationException)
+        {
+            // given
+            string somePath = GetRandomString();
+            string someContent = GetRandomString();
+
+            var invalidFileDependencyException =
+                new InvalidFileServiceDependencyException(
+                    dependencyValidationException);
+
+            var expectedDependencyValidationException =
+                new FileServiceDependencyValidationException(
+                    invalidFileDependencyException);
+
+            this.fileSystemBrokerMock.Setup(broker =>
+                broker.WriteToFile(somePath, someContent))
+                    .Throws(dependencyValidationException);
+
+            // when
+            Action writeToFileAction = () =>
+                this.fileService.WriteToFile(somePath, someContent);
+
+            // then
+            Assert.Throws<FileServiceDependencyValidationException>(
+                writeToFileAction);
+
+            this.fileSystemBrokerMock.Verify(broker =>
+                broker.WriteToFile(somePath, someContent),
+                    Times.Once);
+
+            this.fileSystemBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
