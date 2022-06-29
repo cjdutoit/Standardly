4a81489870fea4002774c9f3b37656800279b764
Christo du Toit <christo@dutoit.co.uk>
2022-06-29 20:51:47 +0100

    ShouldCheckIfFileExists -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/FileServices/FileServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Standardly.Core.Brokers.FileSystems;
+using Standardly.Core.Services.Foundations.FileServices;
+using Tynamix.ObjectFiller;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.FileServices
+{
+    public partial class FileServiceTests
+    {
+        private readonly Mock<IFileSystemBroker> fileSystemBrokerMock;
+        private readonly IFileService fileService;
+
+        public FileServiceTests()
+        {
+            fileSystemBrokerMock = new Mock<IFileSystemBroker>();
+            this.fileService = new FileService(fileSystemBroker: this.fileSystemBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}
---
+++ Standardly.Core/Services/Foundations/FileServices/FileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Standardly.Core.Brokers.FileSystems;
+
+namespace Standardly.Core.Services.Foundations.FileServices
+{
+    public partial class FileService : IFileService
+    {
+        private readonly IFileSystemBroker fileSystemBroker;
+
+        public FileService(
+            IFileSystemBroker fileSystemBroker)
+        {
+            this.fileSystemBroker = fileSystemBroker;
+        }
+
+        public bool CheckIfFileExists(string path) =>
+            throw new NotImplementedException();
+    }
+}
