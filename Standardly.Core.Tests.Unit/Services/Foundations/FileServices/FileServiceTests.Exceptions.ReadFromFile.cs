7ee749c300a02bfc2600777ea375fe40ac66053f
Christo du Toit <christo@dutoit.co.uk>
2022-06-30 00:51:13 +0100

    ShouldThrowDependencyValidationExceptionOnReadFromFileIfDependencyValidationErrorOccursAndLogIt -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/FileServices/FileServiceTests.Exceptions.ReadFromFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.FileServices.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.FileServices
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyValidationExceptions))]
+        public void ShouldThrowDependencyValidationExceptionOnReadFromFileIfDependencyValidationErrorOccursAndLogIt(
+            Exception dependencyValidationException)
+        {
+            // given
+            string somePath = GetRandomString();
+
+            var invalidFileServiceDependencyException =
+                new InvalidFileServiceDependencyException(dependencyValidationException);
+
+            var expectedFileServiceDependencyValidationException =
+                new FileServiceDependencyValidationException(invalidFileServiceDependencyException);
+
+            this.fileSystemBrokerMock.Setup(broker =>
+                broker.ReadFile(somePath))
+                    .Throws(dependencyValidationException);
+
+            // when
+            Action writeToFileAction = () =>
+                this.fileService.ReadFromFile(somePath);
+
+            FileServiceDependencyValidationException actualFileServiceDependencyValidationException =
+                Assert.Throws<FileServiceDependencyValidationException>(writeToFileAction);
+
+            // then
+            actualFileServiceDependencyValidationException.Should().BeEquivalentTo(expectedFileServiceDependencyValidationException);
+
+            this.fileSystemBrokerMock.Verify(broker =>
+                broker.ReadFile(somePath),
+                    Times.Once);
+
+            this.fileSystemBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
