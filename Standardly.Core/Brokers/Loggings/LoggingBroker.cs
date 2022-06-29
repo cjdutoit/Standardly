8c04f60d6a2ff86685a4eef1d74fb29df0e9182b
Christo du Toit <christo@dutoit.co.uk>
2022-06-29 19:17:44 +0100

    BROKERS: Logging - Log LogDebug

--- Standardly.Core/Brokers/Loggings/LoggingBroker.cs
+++ Standardly.Core/Brokers/Loggings/LoggingBroker.cs
@@ -15,5 +15,8 @@ namespace Standardly.Core.Brokers.Loggings
 
         public void LogTrace(string message) =>
             this.logger.LogTrace(message);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
     }
 }
