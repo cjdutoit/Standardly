b8524825f1cc03f8d1ee53832917919da8634990
Christo du Toit <christo@dutoit.co.uk>
2022-06-30 01:48:38 +0100

    ShouldThrowValidationExceptionOnCheckIfDirectoryExistsIfPathIsInvalid -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/FileServices/FileServiceTests.Validations.CheckIfDirectoryExists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.FileServices.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.FileServices
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ShouldThrowValidationExceptionOnCheckIfDirectoryExistsIfPathIsInvalid(
+          string invalidPath)
+        {
+            // given
+            var invalidFilePathException =
+                new InvalidFilePathException();
+
+            var expectedFileServiceValidationException =
+                new FileServiceValidationException(invalidFilePathException);
+
+            // when
+            Action checkIfDirectoryExistsAction = () =>
+                this.fileService.CheckIfDirectoryExists(invalidPath);
+
+            FileServiceValidationException actualFileServiceValidationException =
+                Assert.Throws<FileServiceValidationException>(checkIfDirectoryExistsAction);
+
+            // then
+            actualFileServiceValidationException.Should().BeEquivalentTo(expectedFileServiceValidationException);
+
+            this.fileSystemBrokerMock.Verify(broker =>
+                broker.CheckIfDirectoryExists(
+                    It.IsAny<string>()),
+                        Times.Never);
+
+            this.fileSystemBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
