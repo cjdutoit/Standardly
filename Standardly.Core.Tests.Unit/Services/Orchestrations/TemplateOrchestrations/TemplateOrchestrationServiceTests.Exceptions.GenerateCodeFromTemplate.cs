83305761f5e4809a9c9405db01b633fb30be1578
Christo du Toit <christo@dutoit.co.uk>
2022-07-02 23:59:06 +0100

    ShoudThrowServiceExceptionIfServiceErrorOccurs -> FAIL

--- Standardly.Core.Tests.Unit/Services/Orchestrations/TemplateOrchestrations/TemplateOrchestrationServiceTests.Exceptions.GenerateCodeFromTemplate.cs
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/TemplateOrchestrations/TemplateOrchestrationServiceTests.Exceptions.GenerateCodeFromTemplate.cs
@@ -35,6 +35,7 @@ namespace Standardly.Core.Tests.Unit.Services.Orchestrations.TemplateOrchestrati
                 templateService.TransformString(inputTemplate.RawTemplate, randomReplacementDictionary))
                     .Throws(dependencyValidationException);
 
+            // when
             Action generateCodeFromTemplateAction = () =>
                this.templateOrchestrationService.GenerateCodeFromTemplate(inputTemplate, randomReplacementDictionary);
 
@@ -71,6 +72,7 @@ namespace Standardly.Core.Tests.Unit.Services.Orchestrations.TemplateOrchestrati
                 templateService.TransformString(inputTemplate.RawTemplate, randomReplacementDictionary))
                     .Throws(dependencyException);
 
+            // when
             Action generateCodeFromTemplateAction = () =>
                this.templateOrchestrationService.GenerateCodeFromTemplate(inputTemplate, randomReplacementDictionary);
 
@@ -89,5 +91,43 @@ namespace Standardly.Core.Tests.Unit.Services.Orchestrations.TemplateOrchestrati
             this.powerShellServiceMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public void ShoudThrowServiceExceptionIfServiceErrorOccurs()
+        {
+            // given
+            Template randomTemplate = CreateRandomTemplate();
+            Template inputTemplate = randomTemplate;
+            Dictionary<string, string> randomReplacementDictionary = CreateReplacementDictionary();
+
+            var serviceException = new Exception();
+
+            var failedTemplateOrchestrationServiceException =
+                new FailedTemplateOrchestrationServiceException(serviceException);
+
+            var expectedTemplateOrchestrationServiceException =
+                new TemplateOrchestrationServiceException(failedTemplateOrchestrationServiceException);
+
+            this.templateServiceMock.Setup(templateService =>
+                templateService.TransformString(inputTemplate.RawTemplate, randomReplacementDictionary))
+                    .Throws(serviceException);
+
+            // when
+            Action generateCodeFromTemplateAction = () =>
+               this.templateOrchestrationService.GenerateCodeFromTemplate(inputTemplate, randomReplacementDictionary);
+
+            TemplateOrchestrationDependencyException actualException =
+                Assert.Throws<TemplateOrchestrationDependencyException>(generateCodeFromTemplateAction);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTemplateOrchestrationServiceException);
+
+            this.templateServiceMock.Verify(templateService =>
+                templateService.TransformString(inputTemplate.RawTemplate, randomReplacementDictionary),
+                    Times.Once);
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.powerShellServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
