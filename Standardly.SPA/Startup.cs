8cf7c5251e1d19c0ef816f6bc3e8636aca28a658
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-24 16:52:39 +0200

    removed unsed template files

--- Standardly.SPA/Startup.cs
+++ Standardly.SPA/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Standardly.SPA.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,7 +27,6 @@ namespace Standardly.SPA
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
