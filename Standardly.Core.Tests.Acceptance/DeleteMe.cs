6a3e01b8a78c7a0dc0888249206a45e8337aa4d4
ahmed magdi <ahmed.magdi@bgomla.com>
2022-07-26 12:08:12 +0200

    added basic test

---
+++ Standardly.Core.Tests.Acceptance/DeleteMe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+            => true.Should().Be(true);
+    }
+}
