c11188d6c5c5ec989a988a255b131231e9d6a6b9
Christo du Toit <christo@dutoit.co.uk>
2022-10-17 22:02:20 +0100

    CODE RUB: Updated the templates

--- Standardly/source.extension.cs
+++ Standardly/source.extension.cs
@@ -11,7 +11,7 @@ namespace Standardly
         public const string Name = "Standardly";
         public const string Description = @"A code generator that will allow you to generate various things in a 'The Standard' compliant way.";
         public const string Language = "en-US";
-        public const string Version = "0.10";
+        public const string Version = "0.11";
         public const string Author = "Christo du Toit";
         public const string Tags = "Standardly, The Standard, Code Generator, Scaffolding, Productivity Tool";
     }
