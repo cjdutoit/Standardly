dccfef34e22cfbc1469ed3e4f17100c545912de6
Christo du Toit <christo@dutoit.co.uk>
2022-10-26 00:12:34 +0100

    ShouldThrowDependencyValidationOnCreateDirectoryIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.CreateDirectory.cs
@@ -0,0 +1,135 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public void ShouldThrowDependencyValidationOnCreateDirectoryIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+
+            var expectedFileProcessingDependencyValidationException =
+                new FileProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.fileServiceMock.Setup(service =>
+                service.CreateDirectory(inputPath))
+                    .Throws(dependencyValidationException);
+
+            // when
+            System.Action runAction = () =>
+                this.fileProcessingService.CreateDirectory(inputPath);
+
+            // then
+            FileProcessingDependencyValidationException actualException =
+                Assert.Throws<FileProcessingDependencyValidationException>(runAction);
+
+            this.fileServiceMock.Verify(service =>
+                service.CreateDirectory(inputPath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public void ShouldThrowDependencyOnCreateDirectoryIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+
+            var expectedFileProcessingDependencyException =
+                new FileProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.fileServiceMock.Setup(service =>
+                service.CreateDirectory(inputPath))
+                    .Throws(dependencyException);
+
+            // when
+            System.Action runAction = () =>
+                this.fileProcessingService.CreateDirectory(inputPath);
+
+            // then
+            FileProcessingDependencyException actualException =
+                Assert.Throws<FileProcessingDependencyException>(runAction);
+
+            this.fileServiceMock.Verify(service =>
+                service.CreateDirectory(inputPath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingDependencyException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnCreateDirectoryIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+            string inputContent = randomPath;
+
+            var serviceException = new Exception();
+
+            var failedFileProcessingServiceException =
+                new FailedFileProcessingServiceException(serviceException);
+
+            var expectedFileProcessingServiveException =
+                new FileProcessingServiceException(
+                    failedFileProcessingServiceException);
+
+            this.fileServiceMock.Setup(service =>
+                service.CreateDirectory(inputPath))
+                    .Throws(serviceException);
+
+            // when
+            System.Action runAction = () =>
+                this.fileProcessingService.CreateDirectory(inputPath);
+
+            // then
+            FileProcessingServiceException actualException =
+                Assert.Throws<FileProcessingServiceException>(runAction);
+
+            this.fileServiceMock.Verify(service =>
+                service.CreateDirectory(inputPath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingServiveException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
