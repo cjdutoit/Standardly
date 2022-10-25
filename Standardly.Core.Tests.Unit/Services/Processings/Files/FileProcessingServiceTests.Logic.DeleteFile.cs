44ae49230478f64f3711662220623309ab8f1f5b
Christo du Toit <christo@dutoit.co.uk>
2022-10-25 22:06:51 +0100

    ShouldDeleteFile -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Logic.DeleteFile.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldDeleteFile()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+            string randomContent = GetRandomString();
+            string inputContent = randomContent;
+
+            // when
+            this.fileProcessingService.DeleteFile(inputFilePath);
+
+            // then
+            this.fileServiceMock.Verify(service =>
+                service.DeleteFile(inputFilePath),
+                    Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
