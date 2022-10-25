1b8b00e4a0e92401806fe9f81205b970d6e5e7e3
Christo du Toit <christo@dutoit.co.uk>
2022-10-25 21:25:32 +0100

    ShouldReadFromFile -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Logic.ReadFromFile.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldReadFromFile()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+
+            // when
+            this.fileProcessingService.ReadFromFile(inputFilePath);
+
+            // then
+            this.fileServiceMock.Verify(service =>
+                service.ReadFromFile(inputFilePath),
+                    Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
