5bdc546c8f2bdf8c31fbdd45c8881ee791a0cafc
Christo du Toit <christo@dutoit.co.uk>
2022-10-25 17:58:11 +0100

    ShouldThrowValidationExceptionOnIfFilesFolderIsInvalidAndLogItAsync -> FAIL

--- Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Validations.CheckIfFileExists.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Validations.CheckIfFileExists.cs
@@ -4,9 +4,50 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xunit;
+
 namespace Standardly.Core.Tests.Unit.Services.Processings.Files
 {
     public partial class FileProcessingServiceTests
     {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ShouldThrowValidationExceptionOnIfFilesFolderIsInvalidAndLogItAsync(
+            string invalidFilePath)
+        {
+            // given
+            var invalidPathFilesProcessingException =
+                new InvalidPathFileProcessingException();
+
+            var expectedFilesProcessingValidationException =
+                new FileProcessingValidationException(invalidPathFilesProcessingException);
+
+            // when
+            System.Action runAction = () =>
+                this.fileProcessingService.CheckIfFileExists(invalidFilePath);
+
+            FileProcessingValidationException actualException =
+                Assert.Throws<FileProcessingValidationException>(runAction);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFilesProcessingValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.Verify(broker =>
+                broker.CheckIfFileExists(invalidFilePath),
+                    Times.Never);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
