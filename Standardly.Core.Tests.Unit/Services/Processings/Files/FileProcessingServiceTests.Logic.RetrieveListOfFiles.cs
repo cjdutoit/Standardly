874099e7cc220487524dffaf0aea0197cdc04b50
Christo du Toit <christo@dutoit.co.uk>
2022-10-25 22:48:39 +0100

    ShouldRetrieveListOfFiles -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Logic.RetrieveListOfFiles.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveListOfFiles()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+            string randomSearchPattern = GetRandomString();
+            string inputSearchPattern = randomSearchPattern;
+            string[] randomOutput = GetRandomListOfStrings();
+            string[] expectedResult = randomOutput;
+
+            this.fileServiceMock.Setup(service =>
+                service.RetrieveListOfFiles(inputFilePath, inputSearchPattern))
+                    .Returns(expectedResult);
+
+            // when
+            string[] actualResult = this.fileProcessingService.RetrieveListOfFiles(inputFilePath, inputSearchPattern);
+
+            // then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+
+            this.fileServiceMock.Verify(service =>
+                service.RetrieveListOfFiles(inputFilePath, inputSearchPattern),
+                    Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
