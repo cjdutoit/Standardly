f1034b35a4eb18437f7feacaf9e78245a4c2dc5c
Christo du Toit <christo@dutoit.co.uk>
2022-10-25 19:41:30 +0100

    ShouldWriteToFile -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Logic.WriteToFile.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldWriteToFile()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+            string randomContent = GetRandomString();
+            string inputContent = randomContent;
+
+            // when
+            this.fileProcessingService.WriteToFile(inputFilePath, inputContent);
+
+            // then
+            this.fileServiceMock.Verify(service =>
+                service.WriteToFile(inputFilePath, inputContent),
+                    Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
