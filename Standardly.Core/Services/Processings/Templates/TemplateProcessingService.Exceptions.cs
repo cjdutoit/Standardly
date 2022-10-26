ec7ad6f1910c335adb30ec09e57877cb5109ad79
Christo du Toit <christo@dutoit.co.uk>
2022-10-26 16:21:42 +0100

    ShouldThrowDependencyValidationOnConvertStringTemplateIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- Standardly.Core/Services/Processings/Templates/TemplateProcessingService.Exceptions.cs
+++ Standardly.Core/Services/Processings/Templates/TemplateProcessingService.Exceptions.cs
@@ -4,7 +4,9 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using Standardly.Core.Models.Foundations.Templates;
+using Standardly.Core.Models.Foundations.Templates.Exceptions;
 using Standardly.Core.Models.Processings.Templates.Exceptions;
 using Standardly.Core.Services.Processings.Templates;
 using Xeptions;
@@ -25,6 +27,29 @@ namespace Standardly.Core.Services.Foundations.Templates
             {
                 throw CreateAndLogValidationException(invalidContentTemplateProcessingException);
             }
+            catch (TemplateValidationException templateValidationException)
+            {
+                throw CreateAndLogDependencyValidationException(templateValidationException);
+            }
+            catch (TemplateDependencyValidationException templateDependencyValidationException)
+            {
+                throw CreateAndLogDependencyValidationException(templateDependencyValidationException);
+            }
+            catch (TemplateDependencyException templateDependencyException)
+            {
+                throw CreateAndLogDependencyException(templateDependencyException);
+            }
+            catch (TemplateServiceException templateServiceException)
+            {
+                throw CreateAndLogDependencyException(templateServiceException);
+            }
+            catch (Exception exception)
+            {
+                var failedTemplateProcessingServiceException =
+                    new FailedTemplateProcessingServiceException(exception);
+
+                throw CreateAndLogServiceException(failedTemplateProcessingServiceException);
+            }
         }
 
         private TemplateProcessingValidationException CreateAndLogValidationException(Xeption exception)
@@ -35,6 +60,38 @@ namespace Standardly.Core.Services.Foundations.Templates
             this.loggingBroker.LogError(templateProcessingValidationException);
 
             return templateProcessingValidationException;
+        }
+
+        private TemplateProcessingDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+        {
+            var templateProcessingDependencyValidationException =
+                new TemplateProcessingDependencyValidationException(
+                    exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(templateProcessingDependencyValidationException);
+
+            return templateProcessingDependencyValidationException;
+        }
+
+        private TemplateProcessingDependencyException CreateAndLogDependencyException(Xeption exception)
+        {
+            var templateProcessingDependencyException =
+                new TemplateProcessingDependencyException(
+                    exception.InnerException as Xeption);
+
+            this.loggingBroker.LogError(templateProcessingDependencyException);
+
+            return templateProcessingDependencyException;
+        }
+
+        private TemplateProcessingServiceException CreateAndLogServiceException(Xeption exception)
+        {
+            var countryProcessingServiceException = new
+                TemplateProcessingServiceException(exception);
+
+            this.loggingBroker.LogError(countryProcessingServiceException);
+
+            return countryProcessingServiceException;
         }
     }
 }
