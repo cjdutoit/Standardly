bc206890bb7d368be2ba7c4128b32daaa250ec1f
Christo du Toit <christo@dutoit.co.uk>
2022-10-26 14:55:45 +0100

    ShouldConvertStringToTemplateExecution -> FAIL

---
+++ Standardly.Core/Services/Processings/Templates/ITemplateProcessingService.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using Standardly.Core.Models.Foundations.Templates;
+
+namespace Standardly.Core.Services.Processings.Templates
+{
+    public interface ITemplateProcessingService
+    {
+        Template ConvertStringToTemplate(string @string);
+        Template ConvertToTemplate(Template template, Dictionary<string, string> replacementDictionary);
+        void ValidateSourceFiles(Template template);
+    }
+}
---
+++ Standardly.Core/Services/Processings/Templates/TemplateProcessingService.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using Standardly.Core.Brokers.Loggings;
+using Standardly.Core.Models.Foundations.Templates;
+using Standardly.Core.Services.Processings.Templates;
+
+namespace Standardly.Core.Services.Foundations.Templates
+{
+    public partial class TemplateProcessingService : ITemplateProcessingService
+    {
+        private readonly ITemplateService templateService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public TemplateProcessingService(ITemplateService templateService, ILoggingBroker loggingBroker)
+        {
+            this.templateService = templateService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public Template ConvertStringToTemplate(string @string) =>
+            throw new System.NotImplementedException();
+
+        public Template ConvertToTemplate(Template template, Dictionary<string, string> replacementDictionary) =>
+            throw new System.NotImplementedException();
+
+        public void ValidateSourceFiles(Template template) =>
+            throw new System.NotImplementedException();
+    }
+}
