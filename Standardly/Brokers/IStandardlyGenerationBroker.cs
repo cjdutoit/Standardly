b83209f55c34eaaa8f8f6f49c68fcc592c46eee3
Christo du Toit <christo@dutoit.co.uk>
2022-12-13 00:34:17 +0000

    BROKERS: Standardly Generation Broker

---
+++ Standardly/Brokers/IStandardlyGenerationBroker.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Standardly.Core.Models.Orchestrations;
+using Standardly.Models.Events;
+
+namespace Standardly.Brokers
+{
+    public interface IStandardlyGenerationBroker
+    {
+        event EventHandler<ItemProcessedEventArgs> Processed;
+        void GenerateCode(TemplateGenerationInfo templateGenerationInfo);
+    }
+}
---
+++ Standardly/Brokers/StandardlyGenerationBroker.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Standardly.Core.Clients;
+using Standardly.Core.Models.Events;
+using Standardly.Core.Models.Orchestrations;
+using Standardly.Models.Events;
+
+namespace Standardly.Brokers
+{
+    public class StandardlyGenerationBroker : IStandardlyGenerationBroker
+    {
+        private readonly IStandardlyGenerationClient standardlyGenerationClient;
+
+        public StandardlyGenerationBroker()
+        {
+            this.standardlyGenerationClient = new StandardlyGenerationClient();
+            this.standardlyGenerationClient.Processed += ItemProcessed;
+        }
+
+        public event EventHandler<ItemProcessedEventArgs> Processed;
+
+        public void GenerateCode(TemplateGenerationInfo templateGenerationInfo)
+        {
+            this.standardlyGenerationClient.GenerateCode(templateGenerationInfo);
+        }
+
+        private void ItemProcessed(object sender, ProcessedEventArgs e)
+        {
+            OnProcessed(e);
+        }
+
+        protected virtual void OnProcessed(ProcessedEventArgs e)
+        {
+            EventHandler<ItemProcessedEventArgs> handler = Processed;
+            if (handler != null)
+            {
+                handler(
+                    this,
+                    new ItemProcessedEventArgs
+                    {
+                        TimeStamp = e.TimeStamp,
+                        Message = e.Message,
+                        Status = e.Status,
+                        ProcessedItems = e.ProcessedItems,
+                        TotalItems = e.TotalItems,
+                    });
+            }
+        }
+    }
+}
