1cf4850c3ea9ea9a51cf93ff826002f398cb0176
Christo du Toit <christo@dutoit.co.uk>
2023-09-09 12:31:27 +0100

    CODE RUB: Added Commands -> Includes Content Files

---
+++ Standardly/ToolWindows/GenerateCodeToolWindow.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.ToolWindows;
+using Microsoft.VisualStudio.RpcContracts.RemoteUI;
+
+namespace Standardly.ToolWindows
+{
+    /// <summary>
+    /// A sample tool window.
+    /// </summary>
+    [VisualStudioContribution]
+    public class GenerateCodeToolWindow : ToolWindow
+    {
+        private readonly GenerateCodeToolWindowContent content = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateCodeToolWindow" /> class.
+        /// </summary>
+        /// <param name="extensibility">
+        /// Extensibility object instance.
+        /// </param>
+        public GenerateCodeToolWindow(VisualStudioExtensibility extensibility)
+            : base(extensibility)
+        {
+            this.Title = "My Tool Window";
+        }
+
+        /// <inheritdoc />
+        public override ToolWindowConfiguration ToolWindowConfiguration => new()
+        {
+            // Use this object initializer to set optional parameters for the tool window.
+            Placement = ToolWindowPlacement.Floating,
+        };
+
+        /// <inheritdoc />
+        public override Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            // Use InitializeAsync for any one-time setup or initialization.
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public override Task<IRemoteUserControl> GetContentAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult<IRemoteUserControl>(content);
+        }
+
+        /// <inheritdoc />
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                content.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Standardly/ToolWindows/GenerateCodeToolWindowCommand.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+
+namespace Standardly.ToolWindows
+{
+    /// <summary>
+    /// A command for showing a tool window.
+    /// </summary>
+    [VisualStudioContribution]
+    public class GenerateCodeToolWindowCommand : Command
+    {
+        /// <summary>GenerateCodeToolWindowCommand" /> class.
+        /// </summary>
+        /// <param name="extensibility">Extensibility object instance.</param>
+        public GenerateCodeToolWindowCommand(VisualStudioExtensibility extensibility)
+            : base(extensibility)
+        {
+        }
+
+        /// <inheritdoc />
+        public override CommandConfiguration CommandConfiguration => new(displayName: "%Standardly.ToolWindows.GenerateCodeToolWindowCommand.DisplayName%")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. To localize the displayName, add an entry in .vsextension\string-resources.json
+            // and reference it here by passing "%Standardly.ToolWindows.GenerateCodeToolWindowCommand.DisplayName%" as a constructor parameter.
+            Placements = new[] { CommandPlacement.KnownPlacements.ExtensionsMenu },
+            Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
+        };
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            await this.Extensibility.Shell().ShowToolWindowAsync<GenerateCodeToolWindow>(activate: true, cancellationToken);
+        }
+    }
+}
