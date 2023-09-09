402cc37946fc875625d5ab3c21052c8762f3047e
Christo du Toit <christo@dutoit.co.uk>
2023-09-09 18:24:49 +0100

    CODE RUB: Updated Build Script

---
+++ Standardly.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+global using Xunit;
