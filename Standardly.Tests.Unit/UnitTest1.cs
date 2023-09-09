066f0c824c36beb079368fb707c487b46d855c3c
Christo du Toit <christo@dutoit.co.uk>
2023-09-09 18:24:49 +0100

    CODE RUB: Updated Build Script

---
+++ Standardly.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Standardly.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
