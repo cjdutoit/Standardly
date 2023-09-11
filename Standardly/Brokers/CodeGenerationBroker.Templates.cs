98bbfb4f87e0eebd9a01d2e7ad12599b80fb69e5
Christo du Toit <christo@dutoit.co.uk>
2023-09-11 17:16:03 +0100

    BROKERS: Generate Code

---
+++ Standardly/Brokers/CodeGenerationBroker.Templates.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Standardly.Core.Models.Services.Foundations.Templates;
+
+namespace Standardly.Brokers
+{
+    internal partial class CodeGenerationBroker
+    {
+        public async ValueTask<List<Template>> FindAllTemplatesAsync()
+        {
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string templateFolderPath = Path.Combine(Path.GetDirectoryName(assembly), @"Templates");
+            string templateDefinitionFileName = "Template.json";
+
+            return await this.standardlyClient.TemplateClient
+                .FindAllTemplatesAsync(templateFolderPath, templateDefinitionFileName);
+        }
+    }
+}
---
+++ Standardly/Brokers/ICodeGenerationBroker.Templates.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Standardly.Core.Models.Services.Foundations.Templates;
+
+namespace Standardly.Brokers
+{
+    internal partial interface ICodeGenerationBroker
+    {
+        ValueTask<List<Template>> FindAllTemplatesAsync();
+    }
+}
